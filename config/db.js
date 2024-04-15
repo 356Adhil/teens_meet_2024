144f3776715eb8c25b3db30b56b392a1a94413e3
Adhil-Akbar <356adhil@gmail.com>
2024-04-15 12:05:49 +0530

    updated

--- config/db.js
+++ config/db.js
@@ -6,10 +6,7 @@ async function connectDB() {
     const MONGODB_URI = process.env.MONGO_URI;
 
     // Connect to MongoDB
-    await mongoose.connect(MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(MONGODB_URI);
     
     console.log('Connected to MongoDB');
   } catch (error) {
