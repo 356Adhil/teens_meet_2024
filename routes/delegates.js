0dadf6809bd8c647050174ab5d9ba26273542761
Adhil-Akbar <356adhil@gmail.com>
2024-04-21 03:33:10 +0530

    updated

--- routes/delegates.js
+++ routes/delegates.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
-const { createDelegate } = require("../controllers/delegates");
+const { createDelegate, getDelegate } = require("../controllers/delegates");
 const multer = require("multer");
 const upload = multer(); // for parsing multipart/form-data
 
 router.post("/", upload.none(), createDelegate);
+router.get("/", getDelegate);
 
 module.exports = router;
